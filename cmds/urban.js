8574b887d98aba9e6765d4564f890a21a8174983
SirKhaoz <SirKhaoz@users.noreply.github.com>
2020-05-07 22:42:20 +1200

    Updated for discord.js 12.0+ TODO: char limit check on res api

--- cmds/urban.js
+++ cmds/urban.js
@@ -4,10 +4,10 @@ const urbanapi = "http://api.urbandictionary.com/v0/define?term=";
 
 module.exports.run = async (bot, message, args) => {
 	let searchterm = args.join(" ");
-
+	//TODO char limit check on results. Currently can exeed embed length
 	fetch(urbanapi + searchterm).then(res => res.json()).then(r => {
 		if(!r.list[0]) return message.reply("Nothing came up with that search in the urban dictionary.");
-		let urbanEmbed = new Discord.RichEmbed()
+		let urbanEmbed = new Discord.MessageEmbed()
 			.setTitle(`${searchterm}`)
 			.setColor("#1aff1a")
 			.addField("Defenition 1:", `${r.list[0].definition}\nExmaple: ${r.list[0].example}`)
