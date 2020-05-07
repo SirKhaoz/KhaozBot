46ae15be70abf812c80c55ced01a2f09acf52506
SirKhaoz <SirKhaoz@users.noreply.github.com>
2020-05-07 23:51:55 +1200

    Updated for discord.js 12.0+, code clean up and updated TODO

--- cmds/osrshs.js
+++ cmds/osrshs.js
@@ -1,13 +1,13 @@
 const Discord = module.require("discord.js");
 const nodefetch = require("node-fetch");
 const rsapi = require('runescape-api');
-
+//TODO update & check with new OSRS HS version
 module.exports.run = async (bot, message, args) => {
 	//Quick check to see if user has not given a name.
 	if(!args[0]){
 		//Reply letting the user know, then delete the message after 7 seconds.
 		let msg = await message.channel.send("Please enter an OSRS name to search.");
-		msg.delete(7000);
+		msg.delete({timeout:7000});
 		return;
 	}
 
@@ -17,19 +17,19 @@ module.exports.run = async (bot, message, args) => {
 		let skills = r.skills, activities = r.activities;
 
 	    var levelNames = Object.keys(skills).map(objKey => {
-		    return objKey.charAt(0).toUpperCase() + objKey.slice(1); 
+		    return objKey.charAt(0).toUpperCase() + objKey.slice(1);
 		});
 		var levels = Object.keys(skills).map(objKey => {
-		    return skills[objKey].level;  
+		    return skills[objKey].level;
 		});
 		var levelexp = Object.keys(skills).map(objKey => {
-		    return skills[objKey].exp;  
+		    return skills[objKey].exp;
 		});
 		var levelrank = Object.keys(skills).map(objKey => {
-		    return skills[objKey].rank;  
+		    return skills[objKey].rank;
 		});
 
-	    let embed = new Discord.RichEmbed()
+	    let embed = new Discord.MessageEmbed()
 			.setAuthor(`Current Stats For ${args[0].charAt(0).toUpperCase() + args[0].slice(1)}`)
 			.setDescription(`Levels For Skills:`)
 			.setColor("#1874CD")
@@ -40,12 +40,10 @@ module.exports.run = async (bot, message, args) => {
 			embed.addField(levelNames[i], `Level:  ${levels[i]}\nEXP:    ${levelexp[i].toLocaleString()}`, true);
 		}
 
-		message.channel.send({embed :embed});
+		message.channel.send(embed);
 
 	}).catch(async e => {
-		let msg = await message.reply("User not found.");
-		msg.delete(7000);
-		return;
+		return await message.reply("User not found.").then(msg => msg.delete({timeout:7000}));
 	});
 
 }
