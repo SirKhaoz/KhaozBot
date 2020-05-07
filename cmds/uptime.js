6c670efcb493f755b8b893e3fa5cc1132ab152c4
SirKhaoz <SirKhaoz@users.noreply.github.com>
2020-05-08 09:58:59 +1200

    Fixed hours displaying too many. Updated text

--- cmds/uptime.js
+++ cmds/uptime.js
@@ -1,7 +1,7 @@
 module.exports.run = async (bot, message, args) => {
 	let totalSeconds = (bot.uptime / 1000);
 	let days = Math.floor(totalSeconds / 86400);
-	let hours = Math.floor(((totalSeconds - (days * 86400)) / 3600);
+	let hours = Math.floor((totalSeconds - (days * 86400)) / 3600);
 	totalSeconds %= 3600;
 	let minutes = Math.floor(totalSeconds / 60);
 	let seconds = totalSeconds % 60;
