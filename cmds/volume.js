13e4e871162c18196b1d9a1cccfda4d9bcb66226
SirKhaoz <SirKhaoz@users.noreply.github.com>
2020-05-07 19:07:43 +1200

    Updated for discord.js 12.0+

--- cmds/volume.js
+++ cmds/volume.js
@@ -2,32 +2,32 @@ module.exports.run = async (bot, message, args) => {
 	if(args[0]=="default"){
 		if(!args[1] || /[^\d]/.test(args[1]) || args[1] > 30 || args[1] < 1) {
 			let msg = await message.channel.send("Please enter a valid number between 1-30");
-			msg.delete(7000);
+			msg.delete({timeout:7000});
 			return;
 		};
-		
+
 		bot.guildSettings[message.guild.id].botVolume = `${(args[1]/100)}`;
 
 		let msg = await message.reply(`Default volume changed to ${args[1]}.`);
-		msg.delete(7000);
+		msg.delete({timeout:7000});
 		return;
 	}
 	if(!args[0] || /[^\d]/.test(args[0]) || args[0] > 30 || args[0] < 1) {
 		let msg = await message.channel.send("Please enter a valid number between 1-30");
-		msg.delete(7000);
+		msg.delete({timeout:7000});
 		return;
 	}
 	if(!bot.musicguilds[message.guild.id].isPlaying){
 		let msg = await message.channel.send("No bot is currently playing music.");
-		msg.delete(7000);
+		msg.delete({timeout:7000});
 		return;
 	}
 
 	bot.musicguilds[message.guild.id].dispatcher.setVolume((args[0]/100));
 	bot.guildSettings[message.guild.id].botVolume = (args[0]/100);
-	
+
 	let msg = await message.channel.send(`Set volume to ${args[0]}.`);
-	msg.delete(7000);
+	msg.delete({timeout:7000});
 }
 
 module.exports.help = {
