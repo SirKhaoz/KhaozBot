1e4d10d2ecbc60170641f67816ad51e0d134de79
SirKhaoz <SirKhaoz@users.noreply.github.com>
2020-05-07 23:37:15 +1200

    Updated mute/unmute for discord js 12+, updated text

--- cmds/mute.js
+++ cmds/mute.js
@@ -1,37 +1,37 @@
 const Discord = module.require("discord.js");
-const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
 	//Checking permissions of user requesting mute
-	if(!message.member.permissions.has("MUTE_MEMBERS")) return message.reply("You do not have permission to do this.");
-
+	if(!message.member.permissions.has("MUTE_MEMBERS")) return message.reply("You do not have permission to mute people.");
 	//Get the mentioned user, either mention or full ID
 	let toMute = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-
-	//If no specified @ user mention
+	//If no specified @ user mention, return error to user
 	if(!toMute) return message.reply("You did not specify a user mention to mute!");
 	//Prevent muting of bot
-	if(toMute.user.bot) return message.reply("You can not mute a Bot.");
+	if(toMute.user.bot) return message.reply("You can not mute a Bot via this command.");
 	//Prevent muting of admin
-	if(toMute.permissions.has("ADMINISTRATOR")) return message.reply("You can not mute an Admin.");
+	if(toMute.permissions.has("ADMINISTRATOR")) return message.reply("You can not mute an Admin via this command.");
 	//Prevent self-muting
 	if(toMute.id === message.author.id) return message.reply("Why are you trying to mute yourself?");
 	//Prevenet muting of higher or equal role
-	if(toMute.highestRole.position >= message.member.highestRole.position) return message.reply("You can not mute someone with a higher or equal role as yourself.");
+	if(toMute.roles.highest.position >= message.member.roles.highest.position) return message.reply("You can not mute someone with a higher or equal role as yourself.");
 
 	//Attempt to find pre-exsisting role of "Muted"
 	let role = message.guild.roles.cache.find(r => r.name === "Muted");
 	//If no role, create role
 	if(!role){
 		try{
-			role = await message.guild.createRole({
-				name: "Muted",
-				color: "#ff0000",
-				permissions: []
+			role = await message.guild.roles.create({
+				data:{
+					name: "Muted",
+					color: "#ff0000",
+					permissions: []
+				},
+				reason: "Created to mute/unmute people by KhaozBot"
 			});
 
 			message.guild.channels.cache.forEach(async (channel, id) => {
-				await channel.overwritePermissions(role, {
+				await channel.createOverwrite(role, {
 					SEND_MESSAGES: false,
 					ADD_REACTIONS: false
 				});
@@ -43,11 +43,11 @@ module.exports.run = async (bot, message, args) => {
 
 	}
 
-	if(toMute.roles.has(role.id)) return message.reply("This user is already muted!");
+	if(toMute.roles.cache.has(role.id)) return message.reply("This user is already muted! Use !unmute to un-mute them.");
 
 	//Checking to see if additional args was a number
 	if(args[1]) {
-		if(isNaN(parseInt(args[1].trim()))) return message.reply("Please specify a correct amount of time, in seconds.");
+		if(isNaN(parseInt(args[1].trim()))) return message.reply("Please specify a correct amount of time, in seconds, or none for a 'permanent' mute.");
 
 		bot.mutes[toMute.id] = {
 			guild: message.guild.id,
@@ -61,21 +61,18 @@ module.exports.run = async (bot, message, args) => {
 		}
 	}
 
-	await toMute.addRole(role);
+	await toMute.roles.add(role);
 
 	let adminChannel = message.guild.channels.cache.filter(c=>c.id == bot.guildSettings[message.guild.id].adminchannel).first();
-	let muteEmbed = new Discord.RichEmbed()
-		.setDescription("--- MUTED: ---")
+	let muteEmbed = new Discord.MessageEmbed()
+		.setDescription("--- **MUTED:** ---")
 		.setColor("#0099CC")
 		.addField("Muted user:", `${toMute} (${toMute.displayName}) with ID of ${toMute.id}`)
 		.addField("Muted by:", `${message.author}`)
 		.addField("Time:", message.createdAt)
 	adminChannel.send(muteEmbed);
 
-	fs.writeFile("./mutes.json", JSON.stringify(bot.mutes, null, 4), err => {
-		if(err) throw err;
-		message.reply(`I have muted ${toMute} for you.`);
-	});
+	message.reply(`I have muted ${toMute} for you.`);
 }
 
 module.exports.help = {
--- cmds/unmute.js
+++ cmds/unmute.js
@@ -1,42 +1,39 @@
 const Discord = module.require("discord.js");
-const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
-	//Checking permissions:
-	if(!message.member.permissions.has("MUTE_MEMBERS")) return message.reply("You do not have permission to unmute.");
-
-	//Get the mentioned user, return if there is none.
+	//Checking permissions of user requesting mute
+	if(!message.member.permissions.has("MUTE_MEMBERS")) return message.reply("You do not have permission to un-mute people.");
+	//Get the mentioned user, either mention or full ID
 	let toMute = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-	if(!toMute) return message.reply("You did not specify a user mention!");
+	//If no specified @ user mention, return error to user
+	if(!toMute) return message.reply("You did not specify a user mention to un-mute!");
 
+	//Attempt to find pre-exsisting role of "Muted"
 	let role = message.guild.roles.cache.find(r => r.name === "Muted");
-	
-	if(!role || !toMute.roles.has(role.id)) return message.reply(`sorry ${toMute} is not muted.`);
+	//If no role, assume role is deleted and therefore the user is not muted, return message
+	if(!role || !toMute.roles.cache.has(role.id)) return message.reply(`sorry ${toMute} is not muted.`);
 
-	await toMute.removeRole(role);
+	await toMute.roles.remove(role);
 	delete bot.mutes[toMute.id];
 
-	fs.writeFile("./mutes.json", JSON.stringify(bot.mutes), err => {
-		if(err) throw err;
-		console.log(`I have unmuted ${toMute.user.tag}.`);
-	});
+	console.log(`I have un-muted ${toMute.user.tag}.`);
 
 	let adminChannel = message.guild.channels.cache.filter(c=>c.id == bot.guildSettings[message.guild.id].adminchannel).first();
-	let muteEmbed = new Discord.RichEmbed()
-		.setDescription("--- UNMUTED: ---")
+	let muteEmbed = new Discord.MessageEmbed()
+		.setDescription("--- **UN-MUTED:** ---")
 		.setColor("#0099CC")
 		.addField("Unmuted user:", `${toMute} with ID of ${toMute.id}`)
 		.addField("Unmuted by:", `${message.author}`)
 		.addField("Time:", message.createdAt)
 	adminChannel.send(muteEmbed);
 
-	message.reply(`I have unmuted ${toMute} for you.`);
+	message.reply(`I have un-muted ${toMute} for you.`);
 	return;
 }
 
 module.exports.help = {
 	name: "unmute",
 	type: "command",
-	desc: "Unmutes a specified user.",
+	desc: "Un-mutes a specified user.",
 	example: "!unmute [@user]"
 }
