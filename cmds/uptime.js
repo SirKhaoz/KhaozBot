c671d14e2d5d455cdf360f584d105d0528c83e7e
SirKhaoz <SirKhaoz@users.noreply.github.com>
2020-05-08 09:16:27 +1200

    Fixed hours displaying too many. Updated text

--- cmds/uptime.js
+++ cmds/uptime.js
@@ -1,14 +1,14 @@
 module.exports.run = async (bot, message, args) => {
 	let totalSeconds = (bot.uptime / 1000);
 	let days = Math.floor(totalSeconds / 86400);
-	let hours = Math.floor(totalSeconds / 3600);
+	let hours = Math.floor(((totalSeconds - (days * 86400)) / 3600);
 	totalSeconds %= 3600;
 	let minutes = Math.floor(totalSeconds / 60);
 	let seconds = totalSeconds % 60;
 
-	let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+	let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${math.Round(seconds)} seconds`;
 
-	message.reply(`The bot has been up for: ${uptime}\n(since last restart)`);
+	message.reply(`The bot has been up for: ${uptime}\n(since last restart - most likley a forced Discord shard reconnect)`);
 }
 
 module.exports.help = {
