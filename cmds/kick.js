7c9ecd20c32793074cb2ba9885c23052330724ff
SirKhaoz <SirKhaoz@users.noreply.github.com>
2020-05-07 12:33:35 +1200

    Updated for 12.0+ removed sending embed as bot.js does this on event.

--- cmds/kick.js
+++ cmds/kick.js
@@ -1,11 +1,8 @@
-const Discord = module.require("discord.js");
-const fs = require("fs");
-
 module.exports.run = async (bot, message, args) => {
 	//Checking permissions of user requesting mute
 	if(!message.member.permissions.has("KICK_MEMBERS")){
 		let msg = await message.reply("You do not have permission to kick people.");
-		msg.delete(7000);
+		msg.delete({timeout:7000});
 		return;
 	}
 
@@ -20,23 +17,13 @@ module.exports.run = async (bot, message, args) => {
 	//Prevent self-muting
 	if(toKick.id === message.author.id) return message.reply("Why are you trying to kick yourself?");
 	//Prevenet muting of higher or equal role
-	if(toKick.highestRole.position >= message.member.highestRole.position) return message.reply("You can not kick someone with a higher or equal role as yourself.");
+	if(toKick.roles.highest.position >= message.member.roles.highest.position) return message.reply("You can not kick someone with a higher or equal role as yourself.");
 
-	let kickReason = args.join(" ").slice(22);
+	let kickReason = args.join(" ").slice(22); //limit size of reason.
 	if (kickReason == "") kickReason = "No Reason Specified."
-
-	let kickEmbed = new Discord.RichEmbed()
-		.setDescription("--- KICKED: ---")
-		.setColor("#ffff00")
-		.addField("Kicked user:", `${toKick} (${toKick.displayName}) with ID of ${toKick.id}`)
-		.addField("Kicked by:", `${message.author}`)
-		.addField("Time:", message.createdAt)
-		.addField("Reason:", kickReason);
 
 	try{
 		message.guild.member(toKick).kick(`${message.author} kicked with reason: ${kickReason}`);
-		let adminChannel = message.guild.channels.cache.filter(c=>c.id == bot.guildSettings[message.guild.id].adminchannel).first();
-		adminChannel.send(kickEmbed);
 	} catch (e) {
 		message.reply(`ERROR, CONSOLE: ${e}\n<@78306944179245056>, check this out bro.`);
 		console.log(e);
