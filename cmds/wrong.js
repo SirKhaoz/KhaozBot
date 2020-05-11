cb8a919702aebc0a264f77798d9f8f865acf4c18
SirKhaoz <SirKhaoz@users.noreply.github.com>
2020-05-11 22:25:42 +1200

    !wrong updated text and now @mentionable

--- cmds/wrong.js
+++ cmds/wrong.js
@@ -1,10 +1,13 @@
 module.exports.run = async (bot, message, args) => {
-	return message.reply("https://giphy.com/gifs/election2016-donald-trump-election-2016-3oz8xLd9DJq2l2VFtu");
+	let mentioned = message.mentions.users.first()
+	let mmessage = (mentioned == null) ? `<@${message.author.id}> says:` : `<@${message.author.id}> says: ${mentioned}, you are WRONG.`
+	return message.channel.send(`https://giphy.com/gifs/election2016-donald-trump-election-2016-3oz8xLd9DJq2l2VFtu\n
+${mmessage}`);
 }
 
 module.exports.help = {
 	name: "wrong",
 	type: "static",
 	desc: "You. Are. WRONG.",
-	example: "!wrong"
+	example: "!wrong *or* !wrong [@mention]"
 }
