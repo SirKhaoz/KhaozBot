ab99206eae66ed44e73fbe5a84285c68c13c3aa8
SirKhaoz <SirKhaoz@users.noreply.github.com>
2020-05-14 10:40:19 +1200

    Allow bot kicking via !kick, updated text

--- cmds/kick.js
+++ cmds/kick.js
@@ -1,7 +1,7 @@
 module.exports.run = async (bot, message, args) => {
 	//Checking permissions of user requesting mute
 	if(!message.member.permissions.has("KICK_MEMBERS")){
-		let msg = await message.reply("You do not have permission to kick people.");
+		let msg = await message.reply("You do not have permission to !kick people.");
 		msg.delete({timeout:7000});
 		return;
 	}
@@ -10,14 +10,12 @@ module.exports.run = async (bot, message, args) => {
 	let toKick = message.guild.member(message.mentions.users.first());
 	if(!toKick) return message.reply("You did not specify a user mention to kick.");
 
-	//Prevent muting of bot
-	if(toKick.user.bot) return message.reply("You can not kick a Bot via this command.");
-	//Prevent muting of admin
-	if(toKick.permissions.has("ADMINISTRATOR")) return message.reply("You can not kick an admin via this command.");
-	//Prevent self-muting
+	//Prevent kicking of admin
+	if(toKick.permissions.has("ADMINISTRATOR")) return message.reply("You can not kick an admin via the !kick command.");
+	//Prevent self-kicking
 	if(toKick.id === message.author.id) return message.reply("Why are you trying to kick yourself?");
 	//Prevenet muting of higher or equal role
-	if(toKick.roles.highest.position >= message.member.roles.highest.position) return message.reply("You can not kick someone with a higher or equal role as yourself.");
+	if(toKick.roles.highest.position >= message.member.roles.highest.position) return message.reply("You can not !kick someone with a higher or equal role as yourself.");
 
 	let kickReason = args.join(" ").slice(22); //limit size of reason.
 	if (kickReason == "") kickReason = "No Reason Specified."
