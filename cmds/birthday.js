4281c266f85db94ecc392d94648f8bb75757a396
SirKhaoz <SirKhaoz@users.noreply.github.com>
2020-05-13 20:54:51 +1200

    Fixed command descriptions/text

--- cmds/birthday.js
+++ cmds/birthday.js
@@ -1,19 +1,20 @@
 module.exports.run = async (bot, message, args) => {
-	if(bot.authusers[message.member.id].admin != 1) return message.reply(`I'm working on this at the moment. No touchy.`);
-
 	let mentioned = (message.mentions.users.first() == null) ? message.author : message.mentions.users.first()
 	let dateinfo = Date.parse(args[0]);
-	if(Number.isNaN(dateinfo)) return message.reply("Please enter a date in the correct format:\n```!birthday [YYYY-MM-DD] [@mention]```").then(m => m.delete({timeout:7000}).catch(e => console.error(e)));
+	if(Number.isNaN(dateinfo)) return message.reply("Please enter a date in the correct format:\n```!birthday [YYYY-MM-DD] *[@mention]* - optional```").then(m => m.delete({timeout:14000}).catch(e => console.error(e)));
 
 	bot.birthdays[mentioned.id] = {
-		guild: message.guild.id,
-		date: dateinfo
+		originguildid: message.guild.id,
+		date: dateinfo,
+		announcedcurrentyear: false
 	}
+	let reply = (message.mentions.users.first() == null) ? `your` : `${message.mentions.users.first()}'s'`
+	message.reply(`You have set ${reply} birthday date as ${args[0]}.`).then(m => m.delete({timeout:10000}).catch(e => console.error(e)));
 }
 
 module.exports.help = {
 	name: "birthday",
 	type: "static",
-	desc: "Sets a birthday for a user",
-	example: "!birthday"
+	desc: "Sets a birthday for a user, or for yourself.",
+	example: "!birthday [YYYY-MM-DD] *or* !birthday [YYYY-MM-DD] [@mention]"
 }
