e2f0c4beb487185ab9d68e6fa5c5b71767532622
SirKhaoz <SirKhaoz@users.noreply.github.com>
2020-05-25 10:02:44 +1200

    Updated !todo

--- cmds/todo.js
+++ cmds/todo.js
@@ -1,14 +1,13 @@
 module.exports.run = async (bot, message, args) => {
 	return message.channel.send(`List of things I'm working on, if I've remembered to update it:\n\
-\`\`\`md\n1. Update twitch bot to be a bit more aesthetic\n\
+\`\`\`md\n1. Fix twitch bot (Adding OAUTH2 call)\n\
 2. Add !fitness command\n\
 3. Update !slap command to count slaps by/to user\n\
 4. Fix !gear command moving from other channel, solution might be to investigate calling seperately after saving image\n\
 5. Finish !restore command to restore from backup (config)\n\
 6. Update twitch bot to cache user information to reduce api calls (display pics etc)\n\
 7. Text level up? Like mee6 bot\n\
-8. Stop 'maintenance' from showing up twice on boss timer\n\
-9. Update !help to dynamically adjust for max size, display in nice code block??\n\
+8. Update !help to dynamically adjust for max size, display in nice code block??\n\
 \`\`\``);
 }
 
