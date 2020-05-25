d9d3044d6fde9e68f5d358636a47bdb7933dd880
SirKhaoz <SirKhaoz@users.noreply.github.com>
2020-05-25 21:03:49 +1200

    Removed ability to change other's birthdays. (Non-requirement)

--- cmds/birthday.js
+++ cmds/birthday.js
@@ -1,15 +1,13 @@
 module.exports.run = async (bot, message, args) => {
-	let mentioned = (message.mentions.users.first() == null) ? message.author : message.mentions.users.first()
 	let dateinfo = Date.parse(args[0]);
 	if(Number.isNaN(dateinfo)) return message.reply("Please enter a date in the correct format:\n```!birthday [YYYY-MM-DD] *[@mention]* - optional```").then(m => m.delete({timeout:14000}).catch(e => console.error(e)));
 
-	bot.birthdays[mentioned.id] = {
+	bot.birthdays[message.author.id] = {
 		originguildid: message.guild.id,
 		date: dateinfo,
 		announcedcurrentyear: false
 	}
-	let reply = (message.mentions.users.first() == null) ? `your` : `${message.mentions.users.first()}'s'`
-	message.reply(`You have set ${reply} birthday date as ${args[0]}.`).then(m => m.delete({timeout:10000}).catch(e => console.error(e)));
+	message.reply(`You have set your birthday date as ${args[0]}.\nI look forward to celebrating!`).then(m => m.delete({timeout:10000}).catch(e => console.error(e)));
 }
 
 module.exports.help = {
